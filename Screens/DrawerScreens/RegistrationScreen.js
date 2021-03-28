99331dfb64bfd19a0b7f9cdd57b38e21411d7739
Mahalakshmi269 <mahalakshmigirajala@gmail.com>
2021-03-28 18:58:08 +0530

    Registration Screen implemented

--- Screens/DrawerScreens/RegistrationScreen.js
+++ Screens/DrawerScreens/RegistrationScreen.js
@@ -1,25 +1,313 @@
-import React from 'react';
+import React, {useState, createRef} from 'react';
 import {
   StyleSheet,
-  View
+  TextInput,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
-const RegistrationScreen = () => {
-  return(){
-    <View style={styles.containerBackground} >
-      <Text>RegisterScreen</Text>
-    </View>
-  }
-}
+import Loader from './Components/Loader';
+import RadioButton from './Components/RadioButton';
+import DropDownPicker from 'react-native-dropdown-picker';
 
+const RegistrationScreen = () => {
+  const [userName, setUserName] = useState('');
+  const [userAge, setUserAge] = useState('');
+  const [userDob, setUserDob] = useState('');
+  const [userProfession, setUserProfession] = useState('Employed');
+  const [userLocality, setUserLocality] = useState('');
+  const [userNoOfGuests, setNoOfGuests] = useState(0);
+  const [userAddress, setUserAddress] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
+  const [
+    isRegistraionSuccess,
+    setIsRegistraionSuccess
+  ] = useState(false);
+
+  const options = [
+  {
+    key: 'Employed',
+    text: 'Employed',
+  },
+  {
+    key: 'Student',
+    text: 'Student',
+  }
+  ];
+  const ageInputRef = createRef();
+  const addressInputRef = createRef();
+  const dobInputRef = createRef();
+  const professionInputRef = createRef();
+  const localityInputRef = createRef();
+  const noOfGuestsInputRef = createRef();
+  const addressInputRef = createRef();
+
+  const handleSubmitButton = () => {
+    setErrortext('');
+    if (!userName) {
+      this.alert('Please fill Name');
+      return;
+    }
+    if (!userAge) {
+      this.alert('Please fill Age');
+      return;
+    }
+    if (!userDob) {
+      this.alert('Please fill Dob');
+      return;
+    }
+    if (!userLocality) {
+      this.alert('Please fill Locality');
+      return;
+    }
+    if (!userAddress) {
+      this.alert('Please fill Address');
+      return;
+    }
+    //Show Loader
+    setLoading(true);
+    var dataToSend = {
+      name: userName,
+      age: userAge,
+      dob: userDob,
+      profession: userProfession,
+      locality: userLocality,
+      noOfGuests: userNoOfGuests,
+      address: userAddress,
+    };
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    fetch('http://localhost:3000/api/user/register', {
+      method: 'POST',
+      body: formBody,
+      headers: {
+        'Content-Type':
+        'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setLoading(false);
+        console.log(responseJson);
+        if (responseJson.status === 'success') {
+          setIsRegistraionSuccess(true);
+          console.log(
+            'Registration Successful.'
+          );
+        } else {
+          setErrortext(responseJson.msg);
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  };
+  if (isRegistraionSuccess) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#307ecc',
+          justifyContent: 'center',
+        }}>
+        <Text style={styles.successTextStyle}>
+          Registration Successful
+        </Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#307ecc'}}>
+      <Loader loading={loading} />
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <KeyboardAvoidingView enabled>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserName) => setUserName(UserName)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Name"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                ageInputRef.current && ageInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserAge) => setUserAge(UserAge)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Age"
+              placeholderTextColor="#8b9cb5"
+              keyboardType="numeric"
+              ref={ageInputRef}
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                dobInputRef.current &&
+                dobInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserDob) => setUserDob(UserDob)}
+              underlineColorAndroid="#f000"
+              placeholder="YYYY MM DD"
+              placeholderTextColor="#8b9cb5"
+              keyboardType="numeric"
+              ref={dobInputRef}
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                localityInputRef.current &&
+                localityInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View styles={styles.SectionStyle}>
+            <RadioButton 
+              options={options}  
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(Locality) => setUserLocality(Locality)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Locality"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                addressInputRef.current && addressInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <DropDownPicker
+              items={[
+                {label: '0', value: 0, hidden: true},
+                {label: '1', value: 1},
+                {label: '2', value: 2},
+              ]}
+              placeholder="Select No.of Guests"
+              defaultValue={noOfGuests}
+              containerStyle={{height: 40}}
+              style={{backgroundColor: '#fafafa'}}
+              itemStyle={{
+                  justifyContent: 'flex-start'
+              }}
+              dropDownStyle={{backgroundColor: '#fafafa'}}
+              onChangeItem={item => setNoOfGuests(item.value)
+            }
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserAddress) =>
+                setUserAddress(UserAddress)
+              }
+              multiline={true}
+              numberOfLines={4}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Address"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              ref={addressInputRef}
+              returnKeyType="next"
+              onSubmitEditing={Keyboard.dismiss}
+              blurOnSubmit={false}
+            />
+          </View>
+          {errortext != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errortext}
+            </Text>
+          ) : null}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={handleSubmitButton}>
+            <Text style={styles.buttonTextStyle}>REGISTER</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  );
+};
 export default RegistrationScreen;
 
 const styles = StyleSheet.create({
-  containerBackground: {
-    flex: 1,
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
     alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: '#00000040',
-  }
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+  successTextStyle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 30,
+  },
 });
